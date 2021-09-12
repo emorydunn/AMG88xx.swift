0f98162eb2e77986e7bfe8c3c36a5e46b4657ebf
Emory Dunn <emory@emorydunn.com>
2021-09-12 12:39:58 -0700

    Add some basic arguments

--- Sources/amg88-test/main.swift
+++ Sources/amg88-test/main.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  main.swift
 //  
 //
 //  Created by Emory Dunn on 9/11/21.
@@ -9,13 +9,31 @@ import Foundation
 import SwiftyGPIO
 import AMG88xx
 
+let averageMode = CommandLine.arguments.contains("--avg")
+let loopMode = CommandLine.arguments.contains("--loop")
+
 let interface = SwiftyGPIO.hardwareI2Cs(for: .RaspberryPiPlusZero)![1]
 
 let sensor = AMG88(interface)
 
+if averageMode {
+    sensor.enableMovingAverage()
+}
+
 let temp = sensor.readThermistor()
 print("Thermistor Temp:", temp)
 
-let pixels = sensor.readPixels()
-print("Pixels:")
-pixels.logPagedData()
+if loopMode {
+    while true {
+        let pixels = sensor.readPixels()
+        print("Pixels:")
+        pixels.logPagedData()
+        
+        sleep(1)
+    }
+} else {
+    let pixels = sensor.readPixels()
+    print("Pixels:")
+    pixels.logPagedData()
+}
+
