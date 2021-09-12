52e9cb09f2682dd07d199c97d2765b56f436ce67
Emory Dunn <emory@emorydunn.com>
2021-09-12 12:16:56 -0700

    Add a mock sensor an tests

--- Tests/AMG88xxTests/AMG88xxTests.swift
+++ Tests/AMG88xxTests/AMG88xxTests.swift
@@ -3,8 +3,27 @@ import XCTest
 import SwiftyGPIO
 
 final class AMG88xxTests: XCTestCase {
-    func testRegisterTemp() {
-        XCTAssertEqual(UInt16(0b0000_0000_0100).signedMag12ToFloat(), 4)
-        XCTAssertEqual(UInt16(0b1000_0000_0100).signedMag12ToFloat(), -4)
+    func test12BitMagnitude() {
+        XCTAssertEqual(UInt16(0b0000_0000_0100).fromSignedMag12(), 4) // +0.25ºC
+        XCTAssertEqual(UInt16(0b1000_0000_0100).fromSignedMag12(), -4) // -0.25ºC
+    }
+    
+    func test12BitInt() {
+        XCTAssertEqual(UInt16(0b0000_0110_0100).fromTwosCompliment(), 100) // +25ºC
+        XCTAssertEqual(UInt16(0b1111_1001_1100).fromTwosCompliment(), -100) // -25ºC
+    }
+    
+    func testThermistor() {
+        let sensor = AMG88(MockAMG())
+        
+        XCTAssertEqual(sensor.readThermistor(), 25)
+    }
+    
+    func testPixel() {
+        let sensor = AMG88(MockAMG())
+        
+        XCTAssertEqual(sensor.readPixel(at: 0), 25)
+        
+        XCTAssertEqual(sensor.readPixel(at: 2), -25)
     }
 }
---
+++ Tests/AMG88xxTests/MockAMG.swift
@@ -0,0 +1,82 @@
+//
+//  File.swift
+//  
+//
+//  Created by Emory Dunn on 9/12/21.
+//
+
+import Foundation
+import SwiftyGPIO
+
+class MockAMG: I2CInterface {
+    
+    var register: [UInt8]
+    
+    init() {
+        self.register = Array(repeating: 0x00, count: 0xFF)
+        
+        // Set thermistor to +25ºc
+        self.register[0x0E] = 0b1001_0000
+        self.register[0x0F] = 0b0001
+        
+        // Set pixel 1 to +25ºc
+        self.register[0x80] = 0b0110_0100
+        self.register[0x81] = 0b0000
+        
+        // Set pixel 2 to -25ºc
+        self.register[0x82] = 0b1001_1100
+        self.register[0x83] = 0b1111
+    }
+    
+    func isReachable(_ address: Int) -> Bool { return true }
+    
+    func setPEC(_ address: Int, enabled: Bool) { }
+    
+    func readByte(_ address: Int) -> UInt8 {
+        fatalError("No-op in mock interface")
+    }
+    
+    func readByte(_ address: Int, command: UInt8) -> UInt8 {
+        return register[Int(command)]
+    }
+    
+    func readWord(_ address: Int, command: UInt8) -> UInt16 {
+        let low = UInt16(register[Int(command)])
+        let high = UInt16(register[Int(command + 1)]) << 8
+        return (high | low)
+    }
+    
+    func readData(_ address: Int, command: UInt8) -> [UInt8] {
+        return [0]
+    }
+    
+    func readI2CData(_ address: Int, command: UInt8) -> [UInt8] {
+        return [0]
+    }
+    
+    func writeQuick(_ address: Int) {
+        fatalError("No-op in mock interface")
+    }
+    
+    func writeByte(_ address: Int, value: UInt8) {
+        
+    }
+    
+    func writeByte(_ address: Int, command: UInt8, value: UInt8) {
+        
+    }
+    
+    func writeWord(_ address: Int, command: UInt8, value: UInt16) {
+        
+    }
+    
+    func writeData(_ address: Int, command: UInt8, values: [UInt8]) {
+        
+    }
+    
+    func writeI2CData(_ address: Int, command: UInt8, values: [UInt8]) {
+        
+    }
+    
+    
+}
