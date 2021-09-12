9eef8b60ce06acc4d59d4c8c172e3e13a1db260d
Emory Dunn <emory@emorydunn.com>
2021-09-12 12:17:15 -0700

    Add conversion from two's compliment

--- Sources/AMG88xx/Extensions/UInt16+Temp.swift
+++ Sources/AMG88xx/Extensions/UInt16+Temp.swift
@@ -7,13 +7,25 @@
 
 import Foundation
 
-extension UInt16 {
+public extension UInt16 {
     /// Convert a 12-bit signed magnitude value to a floating point number.
     ///
     /// - Parameter value: The 12-bit signed magnitude value.
     /// - Returns: The converted floating point number.
-    func signedMag12ToFloat() -> Float {
+    func fromSignedMag12() -> Float {
         let abs = self & 0x7FF
         return (self & 0x800) != 0 ? 0 - Float(abs) : Float(abs)
+    }
+    
+    /// Convert the two's compliment representation to decimal.
+    /// - Parameter bits: The number of bits in the binary representation.
+    /// - Returns: A floating point number representing the decimal value.
+    func fromTwosCompliment(bits: Int = 12) -> Float {
+        if self >> 11 == 1 {
+            // Convert to a signed int, then a float
+            return Float(Int(self) - 1 << 12)
+        } else {
+            return Float(self)
+        }
     }
 }
